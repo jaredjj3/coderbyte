80f96c2b3c72a2f1fd4f5bee2012df961be53217
jaredjj3 <jared@nuts.com>
2018-09-23 17:03:59 -0400

    Complete ex_oh.rb.

---
+++ ex_oh.rb
@@ -0,0 +1,17 @@
+# Using the Ruby language, have the function ExOh(str) take the str parameter being passed and
+# return the string true if there is an equal number of x's and o's, otherwise return the string
+# false. Only these two letters will be entered in the string, no punctuation or numbers.
+# For example: if str is "xooxxxxooxo" then the output should return false because there are
+# 6 x's and 5 o's. 
+
+def ExOh(str)
+  char_map = str.chars.group_by { |char| char }
+  char_map.fetch("x", []).count == char_map.fetch("o", []).count ? "true" : "false"
+end
+   
+puts ExOh("xo") == "true"
+puts ExOh("xxo") == "false"
+puts ExOh("xxxo") == "false"
+puts ExOh("xxxoo") == "false"
+puts ExOh("xxoo") == "true"
+puts ExOh("") == "true"
